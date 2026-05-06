c1a643bbdd37238c9c20891dddc8bd1470e20902
Prakash K <prakash.change@gmail.com>
2026-05-06 15:44:26 +0530

    create applayout using children

---
+++ src/ui/Header.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to={"/"}>Fast react app</Link>
+    </header>
+  );
+}
+
+export default Header;
