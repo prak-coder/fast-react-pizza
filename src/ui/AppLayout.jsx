23eb974d6cad126fbcdb8ac9314ed65c0a3c8a48
Prakash K <prakash.change@gmail.com>
2026-05-06 16:34:37 +0530

    add loadingindicator while data is loaded

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
+import LoadingIndicator from "./LoadingIndicator";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <LoadingIndicator />}
       <Header />
       <main>
         <Outlet />
